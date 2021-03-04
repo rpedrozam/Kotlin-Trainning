4679b36be3f9d4920f35db1380b7ce223afc2e07
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2021-03-04 16:33:07 -0500

    Variables null and any type

--- 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Principal.kt
+++ 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Principal.kt
@@ -3,7 +3,7 @@ package com.rpedrozam.fundamentals
 
 
 fun main(){
-    
+
     println("Hello World")
 
     newTopic("Excecute my first function")
@@ -17,6 +17,14 @@ fun main(){
     var d = 2
     var e = "String"
     var f: Boolean = true
+
+    //? allow to set null to variable
+    var objNull: String?
+    objNull = null
+
+    //Variable with any type of data
+    var objAnyType: Any?
+    objAnyType = null
 
 }
 
