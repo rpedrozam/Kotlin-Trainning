1ac67608279fa48da1241f1ddfa5adfa4f0e020b
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2021-03-04 16:22:47 -0500

    Variables and constant

--- 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Principal.kt
+++ 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Principal.kt
@@ -3,8 +3,21 @@ package com.rpedrozam.fundamentals
 
 
 fun main(){
+    
     println("Hello World")
+
     newTopic("Excecute my first function")
+
+    newTopic("Variables and constants")
+
+    val a = 2
+    val b = "String"
+    val c = true
+
+    var d = 2
+    var e = "String"
+    var f: Boolean = true
+
 }
 
 //My first function
