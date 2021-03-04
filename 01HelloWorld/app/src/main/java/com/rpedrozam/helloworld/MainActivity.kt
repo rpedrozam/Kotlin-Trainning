a22c71ef4cf368b3aaccab0110248f0e221bb588
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2021-03-03 23:07:30 -0500

    Implementation of other class

--- 01HelloWorld/app/src/main/java/com/rpedrozam/helloworld/MainActivity.kt
+++ 01HelloWorld/app/src/main/java/com/rpedrozam/helloworld/MainActivity.kt
@@ -2,15 +2,30 @@ package com.rpedrozam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    //Variable global, ? indica que la variable puede ser null
+    var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tts = TextToSpeech(this, this)
+
         var message: String = findViewById<TextView>(R.id.TextView).text.toString()
         Log.d("MyTextView", message)
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            findViewById<TextView>(R.id.TextView).text = "Listo!"
+        }else{
+            findViewById<TextView>(R.id.TextView).text = "No disponible!"
+        }
     }
 }
