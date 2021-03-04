0e7d6b21b7d05ee698f8764fed5fa47147bb636e
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2021-03-03 22:59:48 -0500

    Access view from code

--- 01HelloWorld/app/src/main/java/com/rpedrozam/helloworld/MainActivity.kt
+++ 01HelloWorld/app/src/main/java/com/rpedrozam/helloworld/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rpedrozam.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var message: String = findViewById<TextView>(R.id.TextView).text.toString()
+        Log.d("MyTextView", message)
     }
 }
