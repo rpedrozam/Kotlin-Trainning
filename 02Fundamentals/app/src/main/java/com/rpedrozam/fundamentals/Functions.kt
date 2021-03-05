bb15c3aee8ccd4a20318d018188ffdb361237eb8
rpedrozam <43505609+rpedrozam@users.noreply.github.com>
2021-03-04 19:56:54 -0500

    Overload

--- 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Functions.kt
+++ 02Fundamentals/app/src/main/java/com/rpedrozam/fundamentals/Functions.kt
@@ -9,6 +9,11 @@ private fun sayHello() {
     println("Hello world")
     println( "The add result is: ${add(2, 5)}")
     println( "The substract result is: ${substract(2, 5)}")
+
+    showProduct("Gaseosa", "15%", "2 de Febrero")
+    showProduct("Pan", "10%")
+    showProduct("Remolacha")
+    showProduct("Arroz", validity = "15 de Marzo")
 }
 
 private fun add (num1: Int, num2: Int): Int{
@@ -16,3 +21,7 @@ private fun add (num1: Int, num2: Int): Int{
 }
 
 private fun substract (num1: Int, num2: Int) = num1 - num2
+
+private fun showProduct (name: String, promo: String = "sin promoción", validity: String = "agotar existencias"){
+    println("$name = $promo hasta $validity ")
+}
